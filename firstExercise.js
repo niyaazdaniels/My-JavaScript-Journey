5146cbaa6835419d2d19845603e43855ff68acd6
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-11-08 10:21:03 +0200

    Task 3

--- firstExercise.js
+++ firstExercise.js
@@ -12,19 +12,24 @@
 // A. TASK 1:VARIABLES and OPERATORS
 // 1. Create a variable called “language” and assign it the value “JavaScript”. Then create
 // a variable called “score” and assign it the value “10”.
+
     let language = 'Javascript';
     let score = 10;
+
 // 2.Create two variables called “length” and “width”. Assign each variable a value of 10 and 5 respectfully. Use
 // the variables to calculate the area of a rectangle with the length and width specified (remember area = length x width).
+
  let length = 10; 
  let width = 5;
  let area = 10 * 5;
+
 // Write the following to the console using CONCATENATION and BACKTICKS: 
 // “The area of the rectangle is …” where … is the area you calculated.
 console.log(`the area of the rectangle is ` + (area));
 
 // 3.Create two variables called “num1” and “num2.” Assign each variable a value of 10 and 4 respectfully.
 // Calculate and display in the console what the remainder is if num1 is divided by num2.
+
 let num1 = 10;
 let num2 = 4;
 
@@ -42,11 +47,39 @@ let y = num1 % num2;
 console.log((x) + ` remainder `  + (y));
 
 // B. Task 2:Data Types
-// 1. Define the following data types with your own contents[ 'integer', 'decimal', 'string', 'array'(minimum: 3 items), 'Object'(minimum: 4 values)]
+// 1. Define the following data types with your own contents
+// [ 'integer', 'decimal', 'string', 'array'(minimum: 3 items), 'Object'(minimum: 4 values)]
+
+    let integer = -1;
+    let decimal = 1.1;
+    let string = 'Matthew';
+    let array = ['mcdonals','kfc', 'burger king','nandos', 'spur'];
+    
 // 2. Once you’ve created the variables, display them to the console
+
+console.log(integer);
+console.log(decimal);
+console.log(string);
+console.log(array);
+
 // 3. Use the ‘typeof’ function to check each variable
-// 4. Create an array with numbers and strings. Comment what data type you expect this tobe. Then use the ‘typeof’ function on this array
+
+console.log(typeof integer);
+console.log(typeof decimal);
+console.log(typeof string);
+console.log(typeof array);
+
+// 4. Create an array with numbers and strings. Comment what data type you expect this to be. 
+// Then use the ‘typeof’ function on this array
+
+let somethingNice = ['Niyaaz', 'Hungry', 1, 'and', 21, 'this is a string and number'];
+
+console.log(typeof somethingNice);
+
 // 5. Create a null variable and an undefined variable
+
+let age = null;
+let price;
 
 // ***********C. Task 3. functions and contional statements************
 // N.B. READ WITH UNDERSTANDING!!!
