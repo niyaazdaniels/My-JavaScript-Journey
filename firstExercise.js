c5cb6a0e04d29423586f5034f8ea033e8ab71b94
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-11-08 12:39:56 +0200

    Finish

--- firstExercise.js
+++ firstExercise.js
@@ -25,7 +25,7 @@
 
 // Write the following to the console using CONCATENATION and BACKTICKS: 
 // “The area of the rectangle is …” where … is the area you calculated.
-console.log(`the area of the rectangle is ` + (area));
+console.log(`the area of the rectangle is ${area}`);
 
 // 3.Create two variables called “num1” and “num2.” Assign each variable a value of 10 and 4 respectfully.
 // Calculate and display in the console what the remainder is if num1 is divided by num2.
@@ -44,7 +44,7 @@ console.log(num1 / num2);
 let x = num1 / num2;
 let y = num1 % num2;
 
-console.log((x) + ` remainder `  + (y));
+console.log(`${x} remainder ${y}`);
 
 // B. Task 2:Data Types
 // 1. Define the following data types with your own contents
@@ -84,11 +84,88 @@ let price;
 // ***********C. Task 3. functions and contional statements************
 // N.B. READ WITH UNDERSTANDING!!!
 //  1.  Create 3 functions to convert R1000 into dollars, euros and pounds. Take function naming into consideration!
+
+function dollarConvert () {
+    let randAmount = 1000;
+    let dollarCurrency = 18.42;
+    let zarUsd = Math.round(randAmount / dollarCurrency);
+    console.log(`Converted Currency is R${zarUsd}`);
+}
+dollarConvert();
+
+function euroConvert ( ){
+    let randAmount = 1000;
+    let euroCurrency = 19.72;
+    let euroZar = Math.trunc(randAmount / euroCurrency);
+    console.log(`Converted Curency is R${euroZar}`);
+}
+euroConvert();
+
+function poundCovert(){
+    let randAmount = 1000;
+    let poundCurrency = 22.66;
+    let poundZar = Math.trunc(randAmount / poundCurrency);
+    console.log(`Converted Currency is R${poundCurrency}`);
+}
+poundCovert();
+
 //  2.  Create a function that takes a value and calculates the amount entered including VAT (15%), amount entered should be R400.
 //      Then display the message in the console, "The amount after tax is ...".
+
+    function calculateVat (){
+        let amountEntered = 400;
+        let vatAmount = 15;
+        let calculatedAmount = (amountEntered * vatAmount) / 100;
+        console.log(`The Amount of Tax is R${calculatedAmount + amountEntered}`);
+    }
+    calculateVat();
+
 //  3.  Declare 3 variables 1stNum, 2ndNum, 3rdNum and assign values of 8, 20 and 14 respectfully. Now write a function that compares “num1” and “num2” and 
 //      displays the larger value.
+
+        let num1st = 8 ;
+        let num2nd = 20; 
+        let num3rd = 14;
+
+        if (num1st>num2nd) {
+            console.log(`Num1st is Greater`);
+        } else {
+            console.log(`Num2nd is Greater`);
+            
+        }
+
 //  4.  Write a function that determines whether “num1” is odd or even and displays the
 //      result. (Tip: remember the modulus operator %)
-//  5.  Next, write a JavaScript conditional statement to sort the three numbers(1stNum, 2ndNum, 3rdNum) from largest to smallest.
 
+        function oddEven (){
+            if (num1st%2==0) {
+                console.log(`This number is Even`);
+            } else {
+                console.log(`This number is Odd`);
+            }
+        }
+        oddEven();
+
+//  5.  Next, write a JavaScript conditional statement to sort the three numbers(1stNum, 2ndNum, 3rdNum) from largest to smallest.
+        if (num1st>num2nd && num1>num3rd){
+            if(num2nd>num3rd){
+            console.log(`${num1st} , ${num2nd}, ${num3rd}`);
+        }else {
+            console.log(`${num1st}, ${num3rd}, ${num2nd}`);
+        }
+        }
+        else if (num2nd>num1st && num2nd>num3rd) {
+            if(num1st>num3rd){
+                console.log (`${num2nd}, ${num1st}, ${num3rd}`);
+            }else {
+                console.log(`${num2nd}, ${num3rd}, ${num1st}`);
+            }
+        }
+        else if (num3rd>num1st && num3rd>num2nd){
+            if (num1st>num2nd){
+                console.log(`${num3rd}, ${num1st}, ${num2nd}`);
+            }else {
+                console.log(`${num3rd}, ${num2nd}, ${num1st}`);
+            }
+        }
+           
